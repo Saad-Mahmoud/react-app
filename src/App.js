6695a8ba5fcae06eb9c59b113f2629b86c724dfe
Mahmoud Saad <saad.mahmoud@protonmail.com>
2023-01-08 15:45:00 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React from "react";
+import Expenses from "./components/Expenses/Expenses";
+
+const App = () => {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e2",
+      title: "Car Insurance",
+      amount: 799.49,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e3",
+      title: "New TV",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 2, 28),
+    },
+  ];
+
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <Expenses expenses={expenses} />
+    </div>
+  );
+
+  /* return React.createElement(
+    "div",
+    {},
+    React.createElement("h2", {}, "Let's get started!"),
+    React.createElement(Expenses, { expenses: expenses })
+  ); */
+};
+
+export default App;
